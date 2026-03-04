a3c20d5988c44d65d3d20baf60b31aec8147c0e9
Городецкий Никита <98966123+consiglieres@users.noreply.github.com>
2026-03-04 11:00:55 +0500

    Доработанный редактор кода и сервисы

--- frontend/src/pages/home.page/home.page.ts
+++ frontend/src/pages/home.page/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../features/header.component/header.component';
+import { HeaderComponent } from '../../widgets/header.component/header.component';
 import { UserService } from '../../entities/user.service';
-import { CodeEditor } from '../../features/code-editor/code-editor';
+import { CodeEditor } from '../../widgets/code-editor/code-editor';
 
 @Component({
   selector: 'app-home.page',
