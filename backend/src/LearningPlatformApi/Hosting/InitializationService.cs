89b4f6e97ffed79a88c7ef75aa61cba94d10ee0f
Viktor Braidenbikher <v.bra@skbkontur.ru>
2026-03-15 18:01:40 +0500

    feat (backend): добавил логирование в InitializationService

--- backend/src/LearningPlatformApi/Hosting/InitializationService.cs
+++ backend/src/LearningPlatformApi/Hosting/InitializationService.cs
@@ -1,20 +1,44 @@
+using LearningPlatformApi.Persistence.Context;
 using LearningPlatformApi.Persistence.Entities;
 using Microsoft.AspNetCore.Identity;
 
 namespace LearningPlatformApi.Hosting;
 
-public class InitializationService(IServiceScopeFactory scopeFactory, IHostEnvironment hostEnvironment)
+public class InitializationService(IServiceScopeFactory scopeFactory,
+    IHostEnvironment hostEnvironment, ILogger<InitializationService> logger)
     : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        using var scope = scopeFactory.CreateScope();
+        logger.LogInformation("Starting initialization service");
         
+        using var scope = scopeFactory.CreateScope();
+        var applicationContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+
+        try
+        {
+            logger.LogInformation("Ensuring database creation...");
+            await applicationContext.Database.EnsureCreatedAsync(stoppingToken);
+            logger.LogInformation("Database created");
+        }
+        catch (Exception ex)
+        {
+            logger.LogCritical(ex, "Unable to initialize database");
+            throw;
+        }
+        
+        if (!hostEnvironment.IsDevelopment())
+        {
+            logger.LogInformation("Host environment is '{HostEnvironment}', skipping initialization...",
+                hostEnvironment.EnvironmentName);
+        }
+
         var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
-    
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
+
         string[] roles = { "Admin", "Teacher", "Student" };
-    
+
+        logger.LogInformation("Creating base roles [{Roles}]", string.Join(", ", roles));
         foreach (var role in roles)
         {
             if (!await roleManager.RoleExistsAsync(role))
@@ -22,22 +46,24 @@ public class InitializationService(IServiceScopeFactory scopeFactory, IHostEnvir
                 await roleManager.CreateAsync(new IdentityRole(role));
             }
         }
-    
-        // Создание администратора по умолчанию (только для разработки)
+
+        logger.LogInformation("Creating administrator...");
         if (hostEnvironment.IsDevelopment())
         {
             var adminEmail = "admin@learningplatform.com";
             var adminUser = await userManager.FindByEmailAsync(adminEmail);
-        
+
             if (adminUser == null)
             {
-                adminUser = new AppUser
+                adminUser = new UserEntity
                 {
                     UserName = adminEmail,
                     Email = adminEmail,
-                    EmailConfirmed = true
+                    EmailConfirmed = true,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow
                 };
-            
+
                 var result = await userManager.CreateAsync(adminUser, "Admin123!");
                 if (result.Succeeded)
                 {
@@ -45,5 +71,7 @@ public class InitializationService(IServiceScopeFactory scopeFactory, IHostEnvir
                 }
             }
         }
+
+        logger.LogInformation("Initialization service completed");
     }
 }
