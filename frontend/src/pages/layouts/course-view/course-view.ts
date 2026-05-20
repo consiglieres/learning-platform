e2f958e5929daef931ee288a269e564c92bef6fa
Городецкий Никита <98966123+consiglieres@users.noreply.github.com>
2026-05-20 10:25:03 +0500

    course page complite

--- frontend/src/pages/layouts/course-view/course-view.ts
+++ frontend/src/pages/layouts/course-view/course-view.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from '../../../widgets/header.component/header.component';
+import {FooterComponent} from '../../../widgets/footer.component/footer.component';
 
 @Component({
   selector: 'app-course-view',
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './course-view.html',
   styleUrl: './course-view.scss',
